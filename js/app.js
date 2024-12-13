fe3abcb847d518702ec53d393332d97a501b1e49
Melissa <melsublimados@gmail.com>
2024-12-12 22:21:11 -0300

    Tercer preentrega

--- js/app.js
+++ js/app.js
@@ -1,45 +1,20 @@
-class Producto {
-    constructor(codigo = 0, nombre = " ", categoria = " ", precio = 0) {
-        this.codigo = codigo;
-        this.nombre = nombre;
-        this.categoria = categoria;
-        this.precio = precio;
-    }
-    precioConIVA() {
-        return this.precio * 1.21;
-    }
-}
-
 const productos = [
-    new Producto(1, 'Taza de polímero', 'Tazas', 6000),
-    new Producto(2, 'Taza de céramica', 'Tazas', 10000),
-    new Producto(3, 'Portaretrato simple', 'Cuadros', 6500),
-    new Producto(4, 'Portaretrato doble', 'Cuadros', 8500),
-    new Producto(5, 'Cuaderno', 'Librería', 20000),
-    new Producto(6, 'Agenda', 'Librería', 22000),
-    new Producto(7, 'Tarjetas', 'Papelería', 3500),
-    new Producto(8, 'Etiquetas', 'Papelería', 3500)
+    {
+        id: "01",
+        titulo: 'Curriculum Vitae',
+        imagen: "img/cv.png",
+        precio: 3000
+    },
+    {
+        id: "02",
+        titulo: 'Tarjeta Personal',
+        imagen: "img/tarjeta.png",
+        precio: 3500
+    },
+    {
+        id: "03",
+        titulo: 'Invitación Personalizada',
+        imagen: "img/invitacion.png",
+        precio: 4000
+    },
 ];
-
-// NUEVO ARRAY CON EL PRECIO CON IVA
-
-const productosConIVA = productos.map(producto => {
-    return {
-        precioConIVA: producto.precioConIVA()
-    };
-});
-
-// FILTRADO POR CATEGORÍA PARA EN UN FUTURO ARMAR LA PAGE DE CADA UNA
-
-const categoriaTazas = productos.filter(producto => producto.categoria == "Tazas");
-const categoriaCuadros = productos.filter(producto => producto.categoria == "Cuadros");
-const categoriaLibreria = productos.filter(producto => producto.categoria == "Librería");
-const categoriaPapeleria = productos.filter(producto => producto.categoria == "Papelería");
-
-console.log(categoriaTazas);
-console.log(categoriaCuadros);
-console.log(categoriaLibreria);
-console.log(categoriaPapeleria);
-
-
-
